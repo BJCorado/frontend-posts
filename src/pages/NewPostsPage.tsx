39c506eb7d56371b7aa341c10a1541e8c4574668
BJCorado <brayncorado454@gmail.com>
2025-09-27 16:01:43 -0300

    ..

--- src/pages/NewPostsPage.tsx
+++ src/pages/NewPostsPage.tsx
@@ -9,7 +9,7 @@ export default function NewPostsPage() {
   return (
     <div className="p-6 max-w-2xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Nuevo post</h1>
-      <PostForm submitText="Crear" onSubmit={async (data)=>{ await create(data); nav('/posts') }} />
+      <PostForm submitText="Crear" onSubmit={async (data)=>{ await create(data); nav('/') }} />
     </div>
   )
 }
